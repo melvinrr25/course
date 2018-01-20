2c819f0cde4cd4d22f20d28a10a618f1baf82eba
Melvin Rodriguez <melvinrr25@gmail.com>
2018-01-20 16:35:47 -0600

    Initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+function Test0(a, b, c){
+  return a + b + c;
+}
+
+function Test1(a, b, c){
+  return a + b + c;
+}
+
+function Test2(a, b, c){
+  return a + b + c;
+}
+
+function Test3(a, b, c){
+  return a + b + c;
+}
+
+
